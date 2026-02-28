c1572150a3e77c4d435b85d6f653351eb0246af0
Kemilly Alves <234700020+kemilly-alves@users.noreply.github.com>
2026-02-28 17:45:15 -0300

    Removendo arquivo

--- Seção 1/5_String_avancado/script (2).js
+++
@@ -1,6 +0,0 @@
-console.log("Primeira linha \n Segunda linha"); /* Quebra de linha */ 
-console.log("o meu nome é 'Matheus'"); teste
-console.log(O nome dela é "Roberta"');
-// console.log(`A multiplicação de 5 por 3 é ${5*3}`);
-// console.log("O" + "meu" + "nome" + "é" + "Matheus."); 
-
