49220e2aab0efe79eef6e79bd2cbbe41e897eb06
Kemilly Alves <234700020+kemilly-alves@users.noreply.github.com>
2026-03-04 16:11:55 -0300

    Adicionando o último desafio/exercicio

--- Seção 2/5_exercicio_05/script.js
+++ Seção 2/5_exercicio_05/script.js
@@ -4,14 +4,3 @@ console.log("teste" * null); //NaN
 
 // Resposta do professor ao exercício:
 console.log(415 * "Não é numero");  //NaN 
-
-console.log( 5 > 2);
-console.log(typeof (5 > 2));
-
-let isActive = true;
-console.log(isActive);
-console.log(typeof isActive);
-
-let isActivee = (5 > 2);
-console.log(isActivee);
-console.log(typeof isActivee);
---
+++ Seção 2/6_exercicio_06/script.js
@@ -0,0 +1,11 @@
+// Impressão de booleano - Crie uma variável chamada isActive com o valor booleano de true
+console.log( 5 > 2);
+console.log(typeof (5 > 2));
+
+let isActivee = true;
+console.log(isActivee);
+console.log(typeof isActivee);
+
+let isActive = (5 > 2);
+console.log(isActive);
+console.log(typeof isActive);
